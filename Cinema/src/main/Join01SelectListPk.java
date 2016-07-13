2505e503ade33b5f9254f2eb6e67eb6545536dbc
Younghwi <Younghwi@Younghwi>
2016-07-13 12:36:32 +0900

    Join 성공! 이전 버전은 다 지울게요~

--- Cinema/src/main/Join01SelectListPk.java
+++ Cinema/src/main/Join01SelectListPk.java
@@ -7,9 +7,7 @@ import model.Join01DAO;
 
 public class Join01SelectListPk {
 	public static void main(String[] args) {
-		System.out.println("아이디별");
-		System.out.println("장워니니니니니니니니니니");
-		System.out.println("adsfasdfasdfadsfadsfasdf");
+		System.out.println("아이디별 조회");
 		System.out.println("예매번호, 예매자, 예매영화, 예매일자, 상영일자, 상영회차, 상영관, 예매좌석 조회");
 		
 		Join01DAO jdao = new Join01DAO();
