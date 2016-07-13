2f41ab46d8d797e65dedfe25f5089473ac834f36
Younghwi <Younghwi@Younghwi>
2016-07-13 12:42:31 +0900

    Join01 성공!

--- Cinema/src/main/Join01SelectListPk.java
+++ Cinema/src/main/Join01SelectListPk.java
@@ -7,7 +7,7 @@ import model.Join01DAO;
 
 public class Join01SelectListPk {
 	public static void main(String[] args) {
-		System.out.println("아이디별 조회 하겠습니다");
+		System.out.println("아이디별 조회 하겠습니다ㅋㅋ ");
 		System.out.println("예매번호, 예매자, 예매영화, 예매일자, 상영일자, 상영회차, 상영관, 예매좌석 조회");
 		
 		Join01DAO jdao = new Join01DAO();
