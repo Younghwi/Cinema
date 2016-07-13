5b70a901af6432f989c8de6e36932f191a95c4fd
Student <Student@ITFE-7>
2016-07-13 17:06:37 +0900

    헬로

--- Cinema/src/main/Join01SelectListPk.java
+++ Cinema/src/main/Join01SelectListPk.java
@@ -9,6 +9,7 @@ public class Join01SelectListPk {
 	public static void main(String[] args) {
 		System.out.println("아이디별 조회 하겠습니다ㅋㅋ ");
 		System.out.println("예매번호, 예매자, 예매영화, 예매일자, 상영일자, 상영회차, 상영관, 예매좌석 조회");
+		System.out.println("시스템 가동 준비 완료");
 		
 		Join01DAO jdao = new Join01DAO();
 		String id = "cufe";
