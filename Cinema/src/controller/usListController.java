2f06559918cc1cad17a67e9c12527cae14ccc125
Student <Student@ITFE-7>
2016-07-21 09:13:35 +0900

    컨트롤러 업데이트

---
+++ Cinema/src/controller/usListController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class usListController implements SuperController{
+
+	@Override
+	public void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
